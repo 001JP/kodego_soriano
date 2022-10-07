5e7b47f239966cacf3f5c926c3c1b9142e446eef
JP <noob@001.dev>
2022-10-07 19:34:07 +0800

    update important information to val

--- src/main/kotlin/excercise_1/Main.kt
+++ src/main/kotlin/excercise_1/Main.kt
@@ -2,51 +2,54 @@ package excercise_1
 
 class Main {
 
-//    Application for Employment
+        //    Application for Employment
 
-    var position = "Junior App Developer"
-    var desiredSalary = 100000000
-    var dateAvailForWork = "02/01/2023"
+        var position = "Junior App Developer"
+        var desiredSalary = 100000000
+        var dateAvailForWork = "02/01/2023"
 
-//    Personal Information
+        //    Personal Information
 
-    var lastName = "SORIANO"
-    var firstName = "JP"
-    var middle = "MIDDLE NAME"
-    var birthDate = "01/11/2004"
+        val lastName = "SORIANO"
+        val firstName = "JP"
+        val middle = "MIDDLE NAME"
+        val birthDate = "01/11/2004"
 
-    var address = "35"
-    var city = "SAN FERNANDO"
-    var state = "LA UNION"
-    var zip = "2500"
+        var address = "35"
+        var city = "SAN FERNANDO"
+        var state = "LA UNION"
+        var zip = "2500"
 
-    var phone = "09061234567"
-    var email = "noobs@001.com"
-    var sss: Int = 1234567890
-    var usCitizen: Boolean = false
-    var felony: Boolean = true
-    var drugTest: Boolean = true
+        var phone = "09061234567"
+        var email = "noobs@001.com"
+        var sss: Int = 1234567890
+        var usCitizen: Boolean = false
+        var felony: Boolean = true
+        var drugTest: Boolean = true
 
 
-//Education
+        //Education
 
-    var primarySchool = "PRIMARY SCHOOL"
-    var primarySchoolLocation = "PRIMARY SCHOOL LOCATION"
-    var primarySchoolYears = "PRIMARY SCHOOL YEARS"
-    var primarySchoolDegree = "PRIMARY SCHOOL DEGREE"
-    var primarySchoolMajor = "PRIMARY SCHOOL MAJOR"
+        var primarySchool = "PRIMARY SCHOOL"
+        var primarySchoolLocation = "PRIMARY SCHOOL LOCATION"
+        var primarySchoolYears = "PRIMARY SCHOOL YEARS"
+        var primarySchoolDegree = "PRIMARY SCHOOL DEGREE"
+        var primarySchoolMajor = "PRIMARY SCHOOL MAJOR"
 
-    var secondarySchool = "SECONDARY SCHOOL"
-    var secondarySchoolLocation = "SECONDARY SCHOOL LOCATION"
-    var secondarySchoolYears = "SECONDARY SCHOOL YEARS"
-    var secondarySchoolDegree = "SECONDARY SCHOOL DEGREE"
-    var secondarySchoolMajor = "SECONDARY SCHOOL DEGREE"
+        var secondarySchool = "SECONDARY SCHOOL"
+        var secondarySchoolLocation = "SECONDARY SCHOOL LOCATION"
+        var secondarySchoolYears = "SECONDARY SCHOOL YEARS"
+        var secondarySchoolDegree = "SECONDARY SCHOOL DEGREE"
+        var secondarySchoolMajor = "SECONDARY SCHOOL DEGREE"
 
-    var collegeSchool = "COLLEGE SCHOOL"
-    var collegeSchoolLocation = "COLLEGE SCHOOL LOCATION"
-    var collegeSchoolYears = "COLLEGE SCHOOL YEARS"
-    var collegeSchoolDegree = "COLLEGE SCHOOL DEGREE"
-    var collegeSchoolMajor = "COLLEGE SCHOOL MAJOR"
+        var collegeSchool = "COLLEGE SCHOOL"
+        var collegeSchoolLocation = "COLLEGE SCHOOL LOCATION"
+        var collegeSchoolYears = "COLLEGE SCHOOL YEARS"
+        var collegeSchoolDegree = "COLLEGE SCHOOL DEGREE"
+        var collegeSchoolMajor = "COLLEGE SCHOOL MAJOR"
+
+    }
+
 
 
 }
